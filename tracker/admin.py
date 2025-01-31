56fecac7480ace4285c7666ac2120a34e3429c90
veerlakshay <lakshay.rv@gmail.com>
2025-01-30 19:41:36 -0500

    models registered

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, PriceHistory, UserProfile
 
-# Register your models here.
+# Register your models here
+admin.site.register(Product)
+admin.site.register(PriceHistory)
+admin.site.register(UserProfile)
